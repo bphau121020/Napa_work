1c710913c0d97f5283db57a1ae8f3115fa758750
Bui Phuc Hau <54441935+bphau121020@users.noreply.github.com>
2022-10-17 12:34:06 +0700

    upload final code v2

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,11 +13,24 @@ function resetList() {
 }
 
 const load = document.querySelector(".load-more");
+const hide = document.querySelector(".show-less");
 let showCount = 4;
+let hideCount = 7;
 load.addEventListener("click", () => {
   let show = [...document.querySelectorAll(".main__left-content__lists li")];
-  for (let i = showCount; i <= showCount + 5; i++) {
+  for (let i = showCount; i <= showCount + 6; i++) {
     show[i].style.display = "flex";
+    load.style.display = "none";
+    hide.style.display = "flex";
   }
   showCount += 4;
+});
+hide.addEventListener("click", () => {
+  let show = [...document.querySelectorAll(".main__left-content__lists li")];
+  for (let i = hideCount; i <= hideCount + 2 ; i++) {
+    show[i].style.display = "none";
+    load.style.display = "flex";
+    hide.style.display = "none";
+  }
+  hideCount += 0;
 });
